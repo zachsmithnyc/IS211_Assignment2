6b46f37cc8af0377ecc128e839d7a73cbe87a271
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-09-01 14:50:26 -0400

    Programming Assignment 2

--- assignment2.py
+++ assignment2.py
@@ -3,12 +3,33 @@ import urllib.request
 import logging
 import datetime
 
+LOG_FILENAME = 'error.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.debug,)
+
 def downloadData(url):
     """Downloads the data"""
-    pass
+    content = urllib.request.urlopen(url).read().decode('utf-8')
+    return content
 
 def processData(file_content):
-    pass
+    birthday_data = dict()
+
+    for b, line in file_content.split('/n'):
+        if b == o:
+            continue
+        if len(line) == 0
+            continue
+
+        items = line.split(",")
+        ID = int(items[0])
+        name = items[1]
+        try:
+            birthday = datetime.datetime.strptime(elements[2],'%d,%m,%Y')
+        except ValueError:
+            logging.error(f'invalid data: line #{b} for ID #{ID}')
+
+            birthday_data[ID] = (name, birthday)
+
 
 
 def displayPerson(id, personData):
